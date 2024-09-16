b3b13336a78736d3cc31ee903fd596e9077ddd65
Mari <mariannasantiagogodoy58@gmail.com>
2024-09-16 09:32:36 -0300

    Trabalho

---
+++ ShowdoMilhão/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ShowdoMilhão;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ShowdoMilhão/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ShowdoMilhão;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
